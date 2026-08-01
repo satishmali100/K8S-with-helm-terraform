2c6c21f193cd6571d799415f63d8c7cec4ecebfd
Satish Mali <YOUR_GITHUB_EMAIL>
2026-08-01 23:44:33 +0530

    Added use_azuread_auth for backend

--- Environments/non-prod/backend.tf
+++ Environments/non-prod/backend.tf
@@ -3,7 +3,8 @@ terraform {
     resource_group_name  = "satish-rg"
     storage_account_name = "satishstoragesss"
     container_name       = "tfstate"
-    key                  = "nonprod/terraform.tfstate"
+    key                  = "nonprod/terraform.tfstate
+    use_azuread_auth     = true"
   }
 }
 #n#
