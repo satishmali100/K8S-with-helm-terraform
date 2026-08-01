d72b471af686f269c4a1d6c3b2d84fc0cec15b62
Satish Mali <YOUR_GITHUB_EMAIL>
2026-08-01 23:38:12 +0530

    Added remote backend

---
+++ Environments/non-prod/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "satish-rg"
+    storage_account_name = "satishstoragesss"
+    container_name       = "tfstate"
+    key                  = "nonprod/terraform.tfstate"
+  }
+}
