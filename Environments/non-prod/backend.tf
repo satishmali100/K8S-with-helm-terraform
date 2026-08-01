ccbbc9cdcdf309c46ce309c1f59212341f112b06
Satish Mali <YOUR_GITHUB_EMAIL>
2026-08-02 00:32:15 +0530

    new

--- Environments/non-prod/backend.tf
+++ Environments/non-prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "satish-rg"
-    storage_account_name = "satishstoragesss"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "satishtfstate001"   # Same as workflow
     container_name       = "tfstate"
-    key                  = "nonprod.tfstate"
+    key                  = "nonprod/terraform.tfstate"
     use_oidc             = true
   }
 }
